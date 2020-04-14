81b42d8f620ae739b41129e0d34199e5635634bb
Collin Gale <Collin.g086@gmail.com>
2020-04-14 12:22:06 -0600

    Minor Changes

--- Platformer/Assets/Scripts/DGM SCRIPTS/CharacterMovement.cs
+++ Platformer/Assets/Scripts/DGM SCRIPTS/CharacterMovement.cs
@@ -6,11 +6,11 @@ public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
     private Vector3 positionDirection = Vector3.zero;
-    private float gravity = 75f;
+    private float gravity = 9.8f;
     public float speed = 10f;
     public float jumpForce = 0.5f;
     private int maxJump = 1;
-    private int jumpCount = 0;
+    private bool isJumping;
 
     private void Start()
     {
@@ -19,23 +19,23 @@ public class CharacterMovement : MonoBehaviour
 
     void Update()
     {
-        //Moving
+
         positionDirection.x = Input.GetAxis("Horizontal") * speed;
         positionDirection.y -= gravity * Time.deltaTime;
         controller.Move(positionDirection * Time.deltaTime);
 
-        //Jumping
         if (Input.GetButtonDown("Jump"))
         {
-            if (jumpCount < maxJump)
+            if (isJumping == false)
             {
+                Debug.Log("Jumping!");
                 positionDirection.y = jumpForce;
-                jumpCount++;
+                isJumping = true;
             }
         }
         if (controller.isGrounded)
         {
-            jumpCount = 0;
+            isJumping = false;
         }
 
         //Running
