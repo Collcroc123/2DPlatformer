6e044260e8a9a6e250eac19defa99ecb774afaef
Collin Gale <Collin.g086@gmail.com>
2020-04-16 23:22:44 -0600

    Collectable Coins

---
+++ Platformer/Assets/Scripts/Coins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public GameObject player;
+    public Text coinText;
+    public IntData totalCoins;
+    public GameObject coinObj;
+
+    void OnTriggerEnter(Collider other)
+    {
+        totalCoins.value ++;
+        coinText.text = totalCoins.value.ToString();
+        Debug.Log("Coin Collected: " + totalCoins.value);
+        gameObject.SetActive(false);
+    }
+}
