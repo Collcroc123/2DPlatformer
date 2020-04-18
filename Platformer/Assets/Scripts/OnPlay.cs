5b8d234797029b50f3b0aac4414ec05ebe9042d5
Collin Gale <Collin.g086@gmail.com>
2020-04-18 00:23:11 -0600

    Hide menu on play

---
+++ Platformer/Assets/Scripts/OnPlay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnPlay : MonoBehaviour
+{
+    public UnityEvent play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            play.Invoke();
+        }
+    }
+}
