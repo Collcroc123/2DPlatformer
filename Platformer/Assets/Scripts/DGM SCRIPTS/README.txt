1f56d9d1949f2fcdfcb64e35559fa1263f050353
Collin Gale <Collin.g086@gmail.com>
2020-04-20 01:48:43 -0600

    Death and Respawn!
    A huge step! Super proud!

---
+++ Platformer/Assets/Scripts/DGM SCRIPTS/README.txt
@@ -0,0 +1,3 @@
+All of these scripts are not being actively used by the Unity engine. 
+I put these here for referencing in my actual scripts.
+My in-use scripts are located in the directory this folder is in, Assets/Scripts
---
+++ Platformer/Assets/Scripts/Restart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Restart : MonoBehaviour
+{
+    //https://answers.unity.com/questions/1261937/creating-a-restart-button.html
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+     
+}
