6afe4f9273673bcad9d5ea70addea0a4e08fb011
Collin Gale <Collin.g086@gmail.com>
2020-04-20 10:48:39 -0600

    Coin Sounds

--- Platformer/Assets/Scripts/Coins.cs
+++ Platformer/Assets/Scripts/Coins.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Coins : MonoBehaviour
@@ -8,11 +6,14 @@ public class Coins : MonoBehaviour
     public GameObject player;
     public IntData totalCoins;
     public GameObject coinObj;
+    public AudioSource coinSound;
 
     void OnTriggerEnter(Collider other)
     {
+        coinSound.Play(0);
         totalCoins.value ++;
         Debug.Log("Coin Collected: " + totalCoins.value);
+        //gameObject.GetComponent<Renderer>().enabled = false;
         gameObject.SetActive(false);
     }
 }
